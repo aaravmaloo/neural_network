ab02756f2ff100a7b6a9230765c315a00f400fe6
Aarav Maloo <aaravmaloo06@gmail.com>
2025-04-26 13:44:57 +0530

    remove comments

--- src/trainer.py
+++ src/trainer.py
@@ -1,17 +1,13 @@
 from load_images import load_data
 from cnn_model import build_model
 
-# Load dataset
 train_data, val_data = load_data("src/dataset")
 
 
-# Build CNN model
 model = build_model()
 
-# Train model
 epochs = 50
 history = model.fit(train_data, validation_data=val_data, epochs=epochs)
 
-# Save trained model
 model.save("cat_dog_model.h5")
-print("Model saved successfully! 🎉")
+
